382be058978dd860690983a8ee7df077f6d8407e
sliptype <sliptype@gmail.com>
2021-08-30 11:04:29 -0500

    Remove force open of menu items

--- components/NavPopup.tsx
+++ components/NavPopup.tsx
@@ -27,7 +27,7 @@ const NavPopup: React.FC<NavPopupProps> = ({
   const [isPopupVisible, setIsPopupVisible] = useState(false)
   const anchorRef = useRef<HTMLDivElement>()
 
-  const transitions = useTransition(isPopupVisible || true, {
+  const transitions = useTransition(isPopupVisible, {
     from: { opacity: 0, transform: `scale(0)` },
     enter: { opacity: 1, transform: `scale(1)` },
     leave: { opacity: 0, transform: `scale(0)` },
