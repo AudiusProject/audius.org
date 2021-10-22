8e73f5db1d2f6c959803cdd0771a358b9e1440bc
sliptype <sliptype@gmail.com>
2021-10-22 10:15:38 -0500

    Only duplicate header on desktop

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -7,19 +7,24 @@ import Modal from './Modal'
 
 const PageLayout: React.FC = ({ children }) => {
   const [showStickyHeader, setShowStickyHeader] = useState(false)
+  const isDesktop = () =>
+    typeof window !== 'undefined' && window.innerWidth >= 1020
 
+  // Set up scroll listener for sticky header on desktop
   useEffect(() => {
-    const scrollListener = () => {
-      if (window.scrollY > window.innerHeight / 2) {
-        setShowStickyHeader(true)
-      } else {
-        setShowStickyHeader(false)
+    if (isDesktop()) {
+      const scrollListener = () => {
+        if (window.scrollY > window.innerHeight / 2) {
+          setShowStickyHeader(true)
+        } else {
+          setShowStickyHeader(false)
+        }
       }
-    }
-    window.addEventListener('scroll', scrollListener)
+      window.addEventListener('scroll', scrollListener)
 
-    return () => window.removeEventListener('scroll', scrollListener)
-  }, [])
+      return () => window.removeEventListener('scroll', scrollListener)
+    }
+  }, [isDesktop])
 
   return (
     <div>
