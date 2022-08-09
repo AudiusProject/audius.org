ce0a49b830639653fc188a48e1cda218b1c7f189
Raymond Jacobson <ray@audius.co>
2022-08-09 08:56:15 -0700

    [PLAT-267] Update bug bounty

--- pages/[locale]/security/Security.tsx
+++ pages/[locale]/security/Security.tsx
@@ -39,10 +39,9 @@ const Security = () => {
             <h2>Bug Bounty Policy</h2>
             <p>
               <Trans i18nKey='security-disclosure-guidelines'>
-                We generally adhere to HackerOne&apos;s{' '}
-                <a href='https://www.hackerone.com/disclosure-guidelines'>
-                  disclosure guidelines
-                </a>
+                We generally adhere to disclosure guidelines from
+                <a href='https://www.hackerone.com/disclosure-guidelines'>HackerOne</a>
+                and <a href='https://immunefi.com/severity-system/'>Immunefi</a>
                 .
               </Trans>
             </p>
@@ -72,11 +71,35 @@ const Security = () => {
               <li>{t('security-disclosure-process-5')}</li>
             </ol>
             <h2>{t('security-bounty')}</h2>
+            <h3>Smart Contracts (Ethereum, Solana)</h3>
             <ul>
-              <li>{t('security-bounty-1')}</li>
-              <li>{t('security-bounty-2')}</li>
-              <li>{t('security-bounty-3')}</li>
-              <li>{t('security-bounty-4')}</li>
+              <li>{t('security-bounty-smart-contracts-1')}</li>
+              <li>{t('security-bounty-smart-contracts-2')}</li>
+              <li>{t('security-bounty-smart-contracts-3')}</li>
+              <li>{t('security-bounty-smart-contracts-4')}</li>
+            </ul>
+            <h3>Websites & Apps</h3>
+            <ul>
+              <li>{t('security-bounty-web-1')}</li>
+              <li>{t('security-bounty-web-2')}</li>
+              <li>{t('security-bounty-web-3')}</li>
+              <li>{t('security-bounty-web-4')}</li>
+            </ul>
+            <h2>Smart Contract Assets</h2>
+            <ul>
+              <li>Registry: <a href="https://etherscan.io/address/0xd976d3b4f4e22a238c1A736b6612D22f17b6f64C">0xd976d3b4f4e22a238c1A736b6612D22f17b6f64C</a></li>
+              <li>Token: <a href="https://etherscan.io/address/0x18aAA7115705e8be94bfFEBDE57Af9BFc265B998">0x18aAA7115705e8be94bfFEBDE57Af9BFc265B998</a></li>
+              <li>Governance: <a href="https://etherscan.io/address/0x4DEcA517D6817B6510798b7328F2314d3003AbAC">0x4DEcA517D6817B6510798b7328F2314d3003AbAC</a></li>
+              <li>Staking: <a href="https://etherscan.io/address/0xe6D97B2099F142513be7A2a068bE040656Ae4591">0xe6D97B2099F142513be7A2a068bE040656Ae4591</a></li>
+              <li>DelegateManager: <a href="https://etherscan.io/address/0x4d7968ebfD390D5E7926Cb3587C39eFf2F9FB225">0x4d7968ebfD390D5E7926Cb3587C39eFf2F9FB225</a></li>
+              <li>ClaimsManager: <a href="https://etherscan.io/address/0x44617F9dCEd9787C3B06a05B35B4C779a2AA1334">0x44617F9dCEd9787C3B06a05B35B4C779a2AA1334</a></li>
+              <li>ServiceTypeManager: <a href="https://etherscan.io/address/0x9EfB0f4F38aFbb4b0984D00C126E97E21b8417C5">0x9EfB0f4F38aFbb4b0984D00C126E97E21b8417C5</a></li>
+              <li>ServiceProviderFactory: <a href="https://etherscan.io/address/0xD17A9bc90c582249e211a4f4b16721e7f65156c8">0xD17A9bc90c582249e211a4f4b16721e7f65156c8</a></li>
+              <li>EthRewardsManager: <a href="https://etherscan.io/address/0x5aa6B99A2B461bA8E97207740f0A689C5C39C3b0">0x5aa6B99A2B461bA8E97207740f0A689C5C39C3b0</a></li>
+              <li>WormholeClient: <a href="https://etherscan.io/address/0x6E7a1F7339bbB62b23D44797b63e4258d283E095">0x6E7a1F7339bbB62b23D44797b63e4258d283E095</a></li>
+              <li>TrustedNotifierManager: <a href="https://etherscan.io/address/0x6f08105c8CEef2BC5653640fcdbBE1e7bb519D39">0x6f08105c8CEef2BC5653640fcdbBE1e7bb519D39</a></li>
+              <li>Claimable Tokens: <a href="https://explorer.solana.com/address/Ewkv3JahEFRKkcJmpoKB7pXbnUHwjAyXiwEo4ZY2rezQ">Ewkv3JahEFRKkcJmpoKB7pXbnUHwjAyXiwEo4ZY2rezQ</a></li>
+              <li>Rewards Manager: <a href="https://explorer.solana.com/address/DDZDcYdQFEMwcu2Mwo75yGFjJ1mUQyyXLWzhZLEVFcei">DDZDcYdQFEMwcu2Mwo75yGFjJ1mUQyyXLWzhZLEVFcei</a></li>
             </ul>
             <h2>{t('security-not-eligible')}</h2>
             <ul>
@@ -99,10 +122,11 @@ const Security = () => {
                   </a>
                 </Trans>
               </li>
+              <li>{t('security-not-eligible-12')}</li>
             </ul>
           </div>
         </div>
-      </section>
+      </section >
 
       <section className='section bios'>
         <div className='container'>
@@ -137,7 +161,7 @@ const Security = () => {
           data-parallax='6'
         ></span>
       </section>
-    </div>
+    </div >
   )
 }
 
