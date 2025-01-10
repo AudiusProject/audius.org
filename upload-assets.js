943b64adc0dbb02160d40e5469083c9c1c893098
Raymond Jacobson <ray@audius.co>
2025-01-09 17:24:12 -0800

    Add batching

--- upload-assets.js
+++ upload-assets.js
@@ -6,6 +6,33 @@ const path = require('path')
 const directoryPath = path.resolve('./out/_next/static')
 
 const kvNamespace = '7e4963ca204a4783bd576a3b361017eb'
+const BATCH_SIZE = 5 // Adjust this number based on your needs
+
+// Add Semaphore implementation
+class Semaphore {
+  constructor(max) {
+    this.max = max
+    this.current = 0
+    this.queue = []
+  }
+
+  async acquire() {
+    if (this.current < this.max) {
+      this.current++
+      return
+    }
+    await new Promise(resolve => this.queue.push(resolve))
+  }
+
+  release() {
+    if (this.queue.length > 0) {
+      const next = this.queue.shift()
+      next()
+    } else {
+      this.current--
+    }
+  }
+}
 
 function uploadToKV(filePath, kvKey) {
   return new Promise((resolve, reject) => {
@@ -22,24 +49,37 @@ function uploadToKV(filePath, kvKey) {
   });
 }
 
-// Recursive function to process directories
+// Modified processDirectory function
 async function processDirectory(directory) {
-  const items = fs.readdirSync(directory);
-  
-  // Process items sequentially using for...of loop instead of Promise.all
+  const items = fs.readdirSync(directory)
+  const semaphore = new Semaphore(BATCH_SIZE)
+  const uploadPromises = []
+
   for (const item of items) {
     const fullPath = path.join(directory, item)
     
     if (fs.statSync(fullPath).isDirectory()) {
-      // Recursively process subdirectories
-      await processDirectory(fullPath);
+      await processDirectory(fullPath)
     } else {
-      // For files, create KV key relative to ./out
       let kvKey = '_next/' + path.relative('./out/_next', fullPath).replace(/\\/g, '/')
       kvKey = kvKey.replace('[', '%5B').replace(']', '%5D')
-      await uploadToKV(fullPath, kvKey)
+      
+      // Add semaphore-controlled upload
+      const uploadWithSemaphore = async () => {
+        await semaphore.acquire()
+        try {
+          await uploadToKV(fullPath, kvKey)
+        } finally {
+          semaphore.release()
+        }
+      }
+      
+      uploadPromises.push(uploadWithSemaphore())
     }
   }
+
+  // Wait for all uploads in this directory to complete
+  await Promise.all(uploadPromises)
 }
 
 // Start processing from the static directory
